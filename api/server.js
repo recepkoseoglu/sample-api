67043eeb73958b5fcdb262606db036ec6d7bb291
Aras <recepkoseoglu@hotmail.com>
2018-12-16 13:00:34 +0300

    db.json updated and added default limit

--- api/server.js
+++ api/server.js
@@ -4,6 +4,15 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === 'GET') {
+    req.query._limit = req.query._limit || 20;
+  }
+  next();
+});
+
 server.use('/api', router);
 server.listen(3000, () => {
   console.log('JSON Server is running from 3000 port');
